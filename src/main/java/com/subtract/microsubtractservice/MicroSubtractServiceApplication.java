88aad69fb8987da11505e4025e3f6ef8ff995027
ashimgit <ashimnb87@gmail.com>
2019-06-09 14:33:31 +0530

    Subtraction service completed

--- src/main/java/com/subtract/microsubtractservice/MicroSubtractServiceApplication.java
+++ src/main/java/com/subtract/microsubtractservice/MicroSubtractServiceApplication.java
@@ -2,8 +2,10 @@ package com.subtract.microsubtractservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.subtract.controller.**")
 public class MicroSubtractServiceApplication {
 
 	public static void main(String[] args) {
