e4eafb62e758a881d26448181adc3212b7daf50b
ashimgit <ashimnb87@gmail.com>
2019-06-09 14:23:38 +0530

    Added application.yml

---
+++ src/main/java/com/subtract/microsubtractservice/MicroSubtractServiceApplication.java
@@ -0,0 +1,13 @@
+package com.subtract.microsubtractservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSubtractServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSubtractServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subtract/microsubtractservice/MicroSubtractServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subtract.microsubtractservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSubtractServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
